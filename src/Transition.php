d05b8d5d5dd52842b6e582db8a6ce781af69e33b
Bert van Hoekelen <bert@magwel.nl>
2019-11-28 10:34:38 +0100

    :art: Improving structure / format of code

--- src/Transition.php
+++ src/Transition.php
@@ -32,6 +32,11 @@ class Transition
         $this->to = $to;
     }
 
+    public function __toString(): string
+    {
+        return self::getTransitionKey($this->from, $this->to);
+    }
+
     public static function getTransitionKey(TransitionState $from, TransitionState $to): string
     {
         return "{$from}->{$to}";
@@ -55,10 +60,5 @@ class Transition
     public function canBeTransitioned(): bool
     {
         return $this->model->{$this->field} == $this->from && ! $this->isTransitioned;
-    }
-
-    public function __toString(): string
-    {
-        return self::getTransitionKey($this->from, $this->to);
     }
 }
